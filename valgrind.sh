a37076d0fdf01ca7acb2163c3be8454bbf1222f8
thomascswalker <thomascswalker@gmail.com>
2025-12-01 12:39:28 -0500

    remove build

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,1 @@
-source ./run.sh
-
-valgrind  --leak-check=full --show-leak-kinds=all -s ./build/compiler.exe ./examples/file.c8
+valgrind  --leak-check=full --show-leak-kinds=all -s ./build/compiler.exe
