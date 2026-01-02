0d1cc630be217e37af49a945dd0bed9d8c48b8ed
thomascswalker <thomascswalker@gmail.com>
2026-01-02 00:47:35 -0500

    fix movs of different sizes

--- examples/array.asm
+++ examples/array.asm
@@ -1,11 +1,11 @@
 %include "examples/common.asm"
 
 global main
-global array_new
-global array_subscript_set
+global array_malloc
+global array_free
 
 section .data
-    byte_count: dq 0x100                            ; 64-bit
+    array_count: dq 0x100                            ; 64-bit
 
     fmt_int: db "INT: %ld", 10, 0      ; 64-bit integer format
     fmt_malloc_result: db "Allocated %ld bytes at %p", 10, 0
@@ -20,6 +20,11 @@ extern realloc
 extern free
 extern printf
 
+%define CHAR_ 1
+%define SHORT_ 2
+%define INT_ 4
+%define LONG_ 8
+
 %macro PRINT_INT 1
     mov rsi, %1                 
     lea rdi, [rel fmt_int]    
@@ -28,16 +33,22 @@ extern printf
     call printf
 %endmacro
 
-%macro ARRAY_NEW 3
+%macro NEW_ARRAY 3
     ; Parameters: %1 = array name, %2 = array size, %3 = elem_size
     ALIGN_STACK
     ; Load size and multiply by elem_size to get total bytes
     mov rax, QWORD [%2]
     imul rax, %3
     push rax
-    call array_new
+    call array_malloc
     add rsp, 16                 ; Clean up the pushed arguments from the stack
     mov [rel %1], rax    ; Move returned pointer into specified var
+%endmacro
+
+%macro FREE_ARRAY 1
+    mov rax, %1
+    call array_free
+    add rsp, 16
 %endmacro
 
 %macro SET_ARRAY_ELEM 4
@@ -47,13 +58,13 @@ extern printf
     imul rdx, %4
     add rdx, %1
 
-    %if %4 = 1
+    %if %4 = CHAR_
         mov BYTE [rdx], %3
-    %elif %4 = 2
+    %elif %4 = SHORT_
         mov WORD [rdx], %3
-    %elif %4 = 4
-        mov DWORD [rdx], %3
-    %elif %4 = 8
+    %elif %4 = INT_
+        mov DWORD [edx], %3
+    %elif %4 = LONG_
         mov QWORD [rdx], %3
     %else
         ; Invalid element size
@@ -68,13 +79,13 @@ extern printf
     mov rdx, %2
     imul rdx, %4
     add rdx, %1
-    %if %4 = 1
-        movzx %3, BYTE [rdx]
-    %elif %4 = 2
-        movzx %3, WORD [rdx]
-    %elif %4 = 4
-        movzx %3, DWORD [rdx]
-    %elif %4 = 8
+    %if %4 = CHAR_
+        mov %3, BYTE [rdx]
+    %elif %4 = SHORT_
+        mov %3, WORD [rdx]
+    %elif %4 = INT_
+        mov %3, DWORD [edx]
+    %elif %4 = LONG_
         mov %3, QWORD [rdx]
     %else
         ; Invalid element size
@@ -91,13 +102,13 @@ extern printf
     mov rdx, %2
     imul rdx, %3
     add rdx, %1
-    %if %3 = 1                  ; 1-byte (char)
+    %if %3 = CHAR_                
         movzx rsi, BYTE [rdx]
-    %elif %3 = 2                ; 2-bytes (short)
+    %elif %3 = SHORT_               
         movzx rsi, WORD [rdx]
-    %elif %3 = 4                ; 4-bytes (int)
-        movzx rsi, DWORD [rdx]
-    %elif %3 = 8                ; 8-bytes (long)
+    %elif %3 = INT_              
+        mov esi, DWORD [edx]
+    %elif %3 = LONG_              
         mov rsi, QWORD [rdx]
     %else
         ; Invalid element size
@@ -106,41 +117,45 @@ extern printf
         call exit
     %endif
     lea rdi, [rel fmt_int]
-    xor eax, eax
     xor rax, rax
     call printf
 
     mov rax, rbx    ; restore rax
 %endmacro
 
-%define ELEM_SIZE 2
+%define ELEM_SIZE 8
 
 main:
-    ; Create a new array: `array_ptr = malloc(byte_count)`
-    ; element size = 1 (bytes)
-    ARRAY_NEW array_ptr, byte_count, ELEM_SIZE
+    ; Create a new array: `array = malloc(count * sizeof(T));`
+    NEW_ARRAY array_ptr, array_count, ELEM_SIZE
 
-    SET_ARRAY_ELEM array_ptr, 0, 5, ELEM_SIZE    ; array[0] = 5
-    SET_ARRAY_ELEM array_ptr, 1, 10, ELEM_SIZE   ; array[1] = 10
-    SET_ARRAY_ELEM array_ptr, 2, 15, ELEM_SIZE   ; array[2] = 15
-    SET_ARRAY_ELEM array_ptr, 3, 50, ELEM_SIZE   ; array[2] = 15
+    ; Set array elements: `array[index] = value;`
+    SET_ARRAY_ELEM array_ptr, 0, 5, ELEM_SIZE 
+    SET_ARRAY_ELEM array_ptr, 1, 10, ELEM_SIZE
+    SET_ARRAY_ELEM array_ptr, 2, 15, ELEM_SIZE
+    SET_ARRAY_ELEM array_ptr, 3, 50, ELEM_SIZE
 
+    ; Print array elements: `printf("%d\n", array[index]);`
     PRINT_ARRAY_ELEM array_ptr, 0, ELEM_SIZE
     PRINT_ARRAY_ELEM array_ptr, 1, ELEM_SIZE
     PRINT_ARRAY_ELEM array_ptr, 2, ELEM_SIZE
     PRINT_ARRAY_ELEM array_ptr, 3, ELEM_SIZE
 
+    ALIGN_STACK
+    
+    FREE_ARRAY array_free
+
     ; Exit
     mov rdi, 0
     call exit
 
-; array_new
+; array_malloc
 ;
 ; args:
 ;   [rbp + 00]: stack pointer
 ;   [rbp + 08]: return pointer
 ;   [rbp + 16]: size (pushed by caller)
-array_new:
+array_malloc:
     enter
 
     PRINT_INT [ARG1]
@@ -171,4 +186,17 @@ array_new:
 
     mov rax, rbx          ; restore return value
 
+    leave
+
+; array_free
+;
+; args:
+;   [rbp + 00]: stack pointer
+;   [rbp + 08]: return pointer
+;   [rbp + 16]: array pointer
+array_free:
+    enter
+    mov rdi, [ARG1]
+    xor rax, rax
+    call free
     leave
